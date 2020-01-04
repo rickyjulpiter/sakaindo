b58767fe3cecf73f3d3f0ab8a3afad3ed5bf08f0
Andrian Lau <laudron47@gmail.com>
2020-01-04 21:15:12 +0700

    edit padding produk navbar

--- nav.php
+++ nav.php
@@ -8,17 +8,17 @@
 				<img class="d-inline-block align-top logo" src="../sakaindo/assets/SAKAI/logo sakai/sakai logo.png">
 			</a>
             <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
-                    <a href="index" class="nav-link">Home</a>
+                <li class="nav-item" style="padding-right: 26px;">
+                    <a href="index" class="nav-link">HOME</a>
+                </li>
+                <li class="navbar-item" style="padding-right: 26px;">
+                    <a href="tentang" class="nav-link">TENTANG KAMI</a>
+                </li>
+                <li class="navbar-item" style="padding-right: 26px;">
+                    <a href="layanan" class="nav-link">LAYANAN</a>
                 </li>
                 <li class="navbar-item">
-                    <a href="tentang" class="nav-link">Tentang Kami</a>
-                </li>
-                <li class="navbar-item">
-                    <a href="layanan" class="nav-link">Layanan</a>
-                </li>
-                <li class="navbar-item">
-                    <a href="kontak" class="nav-link">Kontak Kami</a>
+                    <a href="kontak" class="nav-link">KONTAK KAMI</a>
                 </li>
             </ul>
         </nav>
--- produk.php
+++ produk.php
@@ -1,33 +1,33 @@
 <div class="col-md-3" id="rcorners4">
-    <h2 style="text-align: center; font-size: 1rem;">Produk Kami</h2>
-    <div class="row">
+    <h2 style="text-align: center; font-size: 1.3rem; font-weight: bold; padding-bottom:25px;">Produk Kami</h2>
+    <div class="row" style="padding-bottom:25px;">
         <div class="col-md-6">
             <img src="../sakaindo/assets/SAKAI/nama produk/ASSET 1.png" class="d-block w-100" alt="..." style="border-radius: 5%;">
-            <h4 style="text-align: center; font-size: 1.2rem;">nama produk</h4>
+            <h4 style="text-align: center; font-size: 1.1rem;">nama produk</h4>
         </div>
         <div class="col-md-6">
             <img src="../sakaindo/assets/SAKAI/nama produk/ASSET 2.png" class="d-block w-100" alt="..." style="border-radius: 5%;">
-            <h4 style="text-align: center; font-size: 1.2rem;">nama produk</h4>
+            <h4 style="text-align: center; font-size: 1.1rem;">nama produk</h4>
         </div>
     </div>
-    <div class="row">
+    <div class="row" style="padding-bottom:25px;">
         <div class="col-md-6">
             <img src="../sakaindo/assets/SAKAI/nama produk/ASSET 3.png" class="d-block w-100" alt="..." style="border-radius: 5%;">
-            <h4 style="text-align: center; font-size: 1.2rem;">nama produk</h4>
+            <h4 style="text-align: center; font-size: 1.1rem;">nama produk</h4>
         </div>
         <div class="col-md-6">
             <img src="../sakaindo/assets/SAKAI/nama produk/ASSET 4.png" class="d-block w-100" alt="..." style="border-radius: 5%;">
-            <h4 style="text-align: center; font-size: 1.2rem;">nama produk</h4>
+            <h4 style="text-align: center; font-size: 1.1rem;">nama produk</h4>
         </div>
     </div>
     <div class="row">
         <div class="col-md-6">
             <img src="../sakaindo/assets/SAKAI/nama produk/ASSET 5.png" class="d-block w-100" alt="..." style="border-radius: 5%;">
-            <h4 style="text-align: center; font-size: 1.2rem;">nama produk</h4>
+            <h4 style="text-align: center; font-size: 1.1rem;">nama produk</h4>
         </div>
         <div class="col-md-6">
             <img src="../sakaindo/assets/SAKAI/nama produk/ASSET 6.png" class="d-block w-100" alt="..." style="border-radius: 5%;">
-            <h4 style="text-align: center; font-size: 1.2rem;">nama produk</h4>
+            <h4 style="text-align: center; font-size: 1.1rem;">nama produk</h4>
         </div>
     </div>
 </div>
